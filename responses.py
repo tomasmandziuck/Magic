c527c061f29e77557191ef9c5df9994c627c5aeb
tomasmandziuck <tom115097@hotmail.com>
2024-08-29 22:06:07 -0300

    Agregar dealers

--- responses.py
+++ responses.py
@@ -19,9 +19,62 @@ def get_response(user_input,user,logger):
         return f"deja de hacerte el boludo {user}"
     elif "encontrame:" in lowered:
         lowered = lowered.replace("encontrame:", "")
-        return f"Resultado Pirulo: \n {get_cards_pirulo(lowered,logger)}\n Resultado Lair:\n{get_cards_lair(lowered,logger)}\n"
+        return f"Pirulo: \n {get_cards_pirulo(lowered,logger)}\n Lair:\n{get_cards_lair(lowered,logger)}\n Dealers:\n {get_cards_dealers(lowered,logger)}"
+        
 
+def get_cards_dealers(input,logger):
+    
+    url = f"https://www.magicdealersstore.com/products/search?q={input}"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"
+    }
+    
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+         #Aquí deberás identificar la estructura de la página HTML y extraer los datos
+        card_list=[]
+        #logger.info(soup)
+        for item in soup.find_all('div', class_="variant-row in-stock"): 
+            if item.find("div",class_="product-price"):
+                continue
+            form = item.find('form', class_='add-to-cart-form')
 
+            
+            data_name = form.get('data-name')
+            data_category = form.get('data-category')
+            data_price = form.get('data-price')
+            data_variant = form.get('data-variant')
+    
+            card_list.append({
+                'titulo': data_name,
+                "variante":data_category + "-" + data_variant,
+                'precio': data_price
+            })
+        logger.info(card_list)
+
+        formatted_list = []
+        for i, card in enumerate(card_list):
+            if i >= 6:  
+                break
+            titulo = card.get('titulo', '')
+            variante = card.get('variante', '')
+            precio = card.get('precio', '')
+            formatted_list.append(f"{titulo}// {variante} // {precio} Patacones")
+
+        if len(card_list) < 6:
+            extra= "\nDisfrute"
+        else:    
+            extra = "\nHay mas pero hay limite de caracteres"
+
+        # Join the formatted items into a single string (optional)
+        formatted_string = "\n".join(formatted_list)    
+        # return formatted_string if formatted_string else "No hay resultados con stock En pirulo"
+        return formatted_string +f"{extra}" if formatted_string else "No hay resultados con stock En Dealers"
+    else:
+        print(f"Error en la solicitud: {response.status_code}")
+        return None
 
 def get_cards_pirulo(input,logger):
     
@@ -54,16 +107,23 @@ def get_cards_pirulo(input,logger):
         logger.info(card_list)
 
         formatted_list = []
-        for card in card_list:
+        for i, card in enumerate(card_list):
+            if i >= 6:  
+                break
             titulo = card.get('titulo', '')
             variante = card.get('variante', '')
             precio = card.get('precio', '')
             formatted_list.append(f"{titulo}// {variante} // {precio} dolarucos")
 
+        if len(card_list) < 6:
+            extra= "\nDisfrute"
+        else:    
+            extra = "\nHay mas pero hay limite de caracteres"    
+
         # Join the formatted items into a single string (optional)
         formatted_string = "\n".join(formatted_list)    
         # return formatted_string if formatted_string else "No hay resultados con stock En pirulo"
-        return formatted_string if formatted_string else "No hay resultados con stock En pirulo"
+        return formatted_string +f"{extra}" if formatted_string else "No hay resultados con stock En pirulo"
     else:
         print(f"Error en la solicitud: {response.status_code}")
         return None
@@ -103,14 +163,21 @@ def get_cards_lair(input,logger,stock=False):
         logger.info(card_list)
 
         formatted_list = []
-        for card in card_list:
+        for i, card in enumerate(card_list):
+            if i >= 6:  
+                break
             titulo = card.get('titulo', '')
             precio = card.get('precio', '')
             formatted_list.append(f"{titulo} // {precio}")
 
+        if len(card_list) < 6:
+            extra= "\nDisfrute"
+        else:    
+            extra = "\nHay mas pero hay limite de caracteres"    
+
         # Join the formatted items into a single string (optional)
         formatted_string = "\n".join(formatted_list)    
-        return formatted_string if formatted_string else "No hay resultados con stock En lair"
+        return formatted_string +f"{extra}" if formatted_string else "No hay resultados con stock En lair"
     else:
         print(f"Error en la solicitud: {response.status_code}")
         return None        
